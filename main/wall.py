8ce9e8eabf17706db4c8a6b235a197e5d19c6d47
abuj43221-wq <abuj43221@gmail.com>
2026-05-14 10:07:35 +0530

    Update wall.py

--- main/wall.py
+++ main/wall.py
@@ -1,28 +1,21 @@
-from main import WALL_API, UNSPLASH_API, session
+from main import WALL_API, UNSPLASH_API
 import aiohttp, random
-
 async def get_wallpapers(query: str):  
   try:
-    url = WALL_API + query  
-    resp = await session.get(url)
-    json = await resp.json()
+    url = WALL_API + query
+    async with aiohttp.ClientSession() as session:
+      resp = await session.get(url)
+      json = await resp.json()
     images = json["images"]
     if len(images) == 0:
       return "nonee " + "Can't find the wallpaper you are trying to search..."
-    random.shuffle(images)
-  except Exception as e:
-    return "error " + str(e)      
-  return images
 
 async def get_unsplash(query: str):  
   try:
-    url = UNSPLASH_API + query  
-    resp = await session.get(url)
-    json = await resp.json()
+    url = UNSPLASH_API + query
+    async with aiohttp.ClientSession() as session:
+      resp = await session.get(url)
+      json = await resp.json()
     images = json["images"]
     if len(images) == 0:
       return "nonee " + "Can't find the wallpaper you are trying to search..."
-    random.shuffle(images)
-  except Exception as e:
-    return "error " + str(e)      
-  return images
