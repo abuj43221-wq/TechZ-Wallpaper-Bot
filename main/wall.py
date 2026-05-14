20768f37148d4167ce01212c1ed299863a3df17d
abuj43221-wq <abuj43221@gmail.com>
2026-05-14 11:12:49 +0530

    Update wall.py

--- main/wall.py
+++ main/wall.py
@@ -1,21 +1,39 @@
 from main import WALL_API, UNSPLASH_API
 import aiohttp, random
-async def get_wallpapers(query: str):  
-  try:
-    url = WALL_API + query
-    async with aiohttp.ClientSession() as session:
-      resp = await session.get(url)
-      json = await resp.json()
-    images = json["images"]
-    if len(images) == 0:
-      return "nonee " + "Can't find the wallpaper you are trying to search..."
 
-async def get_unsplash(query: str):  
-  try:
-    url = UNSPLASH_API + query
-    async with aiohttp.ClientSession() as session:
-      resp = await session.get(url)
-      json = await resp.json()
-    images = json["images"]
-    if len(images) == 0:
-      return "nonee " + "Can't find the wallpaper you are trying to search..."
+async def get_wallpapers(query: str):
+    try:
+        url = WALL_API + query
+
+        async with aiohttp.ClientSession() as session:
+            resp = await session.get(url)
+            data = await resp.json()
+
+        images = data["results"]
+
+        if len(images) == 0:
+            return "nonee Can't find the wallpaper you are trying to search..."
+
+        return images
+
+    except Exception as e:
+        return f"error {e}"
+
+
+async def get_unsplash(query: str):
+    try:
+        url = UNSPLASH_API + query
+
+        async with aiohttp.ClientSession() as session:
+            resp = await session.get(url)
+            data = await resp.json()
+
+        images = data["results"]
+
+        if len(images) == 0:
+            return "nonee Can't find the wallpaper you are trying to search..."
+
+        return images
+
+    except Exception as e:
+        return f"error {e}"
