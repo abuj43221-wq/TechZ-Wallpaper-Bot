acc475c7e2f274741c328bbab579118912d22652
anujedits42-byte <anujedits42@gmail.com>
2026-05-14 08:59:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "34446649"))
+API_HASH = getenv("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
 
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "8967429557:AAHxfH8Ea4FmByOUUpiAtsNh5spg6VUWWqk")
 
 # API By TechZBots || https://t.me/TechZBots
 WALL_API = "https://techzbotsapi.herokuapp.com/wall?query="
 
 UNSPLASH_API = "https://techzbotsapi.herokuapp.com/unsplash?query="
 
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://techz:wall@techzwallbotdb.katsq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
