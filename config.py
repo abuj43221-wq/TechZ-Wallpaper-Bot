e6a83a83c9b24229215cf7a037343ce905650367
abuj43221-wq <abuj43221@gmail.com>
2026-05-15 14:52:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,6 +13,6 @@ BOT_TOKEN = getenv("BOT_TOKEN", "8967429557:AAHxfH8Ea4FmByOUUpiAtsNh5spg6VUWWqk"
 # API By TechZBots || https://t.me/TechZBots
 WALL_API = "https://wall.alphacoders.com/api2.0/get.php?auth=17900fc9b6f655f9d9e39a96a256fcd2&method=search&term="
 
-UNSPLASH_API = "https://techzbotsapi.onrender.com/unsplash?query="
+UNSPLASH_API = "https://api.unsplash.com/search/photos?client_id=7gJGx3SK7iGyxR7RetAgjoS8vWTIqascGXIaf5khPUE&query="
 
 MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
