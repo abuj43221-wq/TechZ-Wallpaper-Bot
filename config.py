6152c3c5de275ce0d9ec9f05586c238b6bc85d87
abuj43221-wq <abuj43221@gmail.com>
2026-05-15 14:15:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
 BOT_TOKEN = getenv("BOT_TOKEN", "8967429557:AAHxfH8Ea4FmByOUUpiAtsNh5spg6VUWWqk")
 
 # API By TechZBots || https://t.me/TechZBots
-WALL_API = "https://techzbotsapi.onrender.com/wall?query="
+WALL_API = "https://wall.alphacoders.com/api2.0/get.php?auth=17900fc9b6f655f9d9e39a96a256fcd2&method=search&term="
 
 UNSPLASH_API = "https://techzbotsapi.onrender.com/unsplash?query="
 
